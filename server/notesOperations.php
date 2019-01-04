c8d5001e344eca1bea0f520717c2c74d2b88a721
Laitooo San <33745947+laitooo@users.noreply.github.com>
2019-01-04 10:37:43 +0200

    Update notesOperations.php

--- server/notesOperations.php
+++ server/notesOperations.php
@@ -18,11 +18,11 @@ class notesOperations
 		$this->con = $db->connectDB();
 	}
 
-	public function addNote( $username , $title , $content ){
+	public function addNote( $id_user , $title , $content ){
 		$num = $this->numNotes() + 1;
-		$stmt = $this->con->prepare("INSERT INTO `notes` (`id`, `username`, `title` , `content`, `date`) 
+		$stmt = $this->con->prepare("INSERT INTO `notes` (`id`, `id_user`, `title` , `content`, `date`) 
 			VALUES ($num,?,?,?,now());");
-		$stmt->bind_param("sss",$username,$title,$content);
+		$stmt->bind_param("sss",$id_user,$title,$content);
 		if($stmt->execute()){
 			return 1;
 		}else{
@@ -46,12 +46,12 @@ class notesOperations
 		return $rowcount;
 	}
 
-	public function findUserNumberOfNotes($username){
+	public function findUserNumberOfNotes($id_user){
 		include_once dirname(__FILE__).'/connect.php';
 		
 		$fff = new connect();
 
-		if ($res = mysqli_query($fff->connectDB(),"SELECT * FROM notes WHERE username = '".$username."'")) {
+		if ($res = mysqli_query($fff->connectDB(),"SELECT * FROM notes WHERE id_user = '".$id_user."'")) {
 			$numN = mysqli_num_rows($res);
 		}
 
@@ -61,6 +61,16 @@ class notesOperations
 	public function editNote($id,$newTitle,$newContent){
 		$stmt = $this->con->prepare("UPDATE notes SET title = ?, content = ? WHERE id = ?");
 		$stmt->bind_param("sss",$newTitle,$newContent,$id);
+		if($stmt->execute()){
+			return 1;
+		}else{
+			return 2;
+		}
+	}
+
+	public function deleteNote($id){
+		$stmt = $this->con->prepare("DELETE notes WHERE id = ?");
+		$stmt->bind_param("s",$id);
 		if($stmt->execute()){
 			return 1;
 		}else{
